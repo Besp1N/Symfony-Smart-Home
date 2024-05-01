c62532101a4e1472ce7ec413ac8f54f98ee662c3
Kacperendo <92695386+Besp1N@users.noreply.github.com>
2024-05-01 21:51:12 +0200

    added backend dashboard

--- src/Controller/HomeConfigController.php
+++ src/Controller/HomeConfigController.php
@@ -2,7 +2,11 @@
 
 namespace App\Controller;
 
+use App\Entity\House;
+use App\Entity\User;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
@@ -12,8 +16,33 @@ class HomeConfigController extends AbstractController
     public function index(): Response
     {
         $user = $this->getUser();
+        $houses = $user instanceof User ? $user->getHouse() : null;
+
+
         return $this->render('home_config/index.html.twig', [
-            'user' => $user
+            'user' => $user,
+            'houses' => $houses
         ]);
+    }
+
+    #[Route('/add-house', name: 'app_add_house')]
+    public function addHouse(Request $request, EntityManagerInterface $entityManager): Response
+    {
+        $user = $this->getUser();
+        $house = new House();
+
+        $houseName = $request->request->get('Name');
+        $houseCity = $request->request->get('City');
+        $houseAddress = $request->request->get('Address');
+
+        $house->setName($houseName);
+        $house->setCity($houseCity);
+        $house->setAddress($houseAddress);
+        $house->setOwner($user);
+
+        $entityManager->persist($house);
+        $entityManager->flush();
+
+        return $this->redirectToRoute('app_home_config');
     }
 }
