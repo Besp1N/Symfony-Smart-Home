31dd348753d434a5a51ccd594b4207e549b724ff
Kacperendo <92695386+Besp1N@users.noreply.github.com>
2024-05-02 14:19:29 +0200

    minor changes

--- src/Controller/HomeConfigController.php
+++ src/Controller/HomeConfigController.php
@@ -5,7 +5,6 @@ namespace App\Controller;
 use App\Entity\House;
 use App\Entity\User;
 use Doctrine\ORM\EntityManagerInterface;
-use Doctrine\ORM\PersistentCollection;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
